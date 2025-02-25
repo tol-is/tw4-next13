dbc13362bc9fa116905473c8dffa23fa0933c239
Tolis C <tchristodoulou@quora.com>
2025-02-25 00:29:36 +0000

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import '@/styles/tailwind.css';
+import "@/styles/tailwind.css";
 
-import type { AppProps } from 'next/app';
-import React from 'react';
+import type { AppProps } from "next/app";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
